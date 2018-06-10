410a75fe902ab2d74d5a2f53e74097d9044e7e5d
LEMANHHUY <habua2911@gmail.com>
2018-06-10 17:15:22 +0700

    ato

---
+++ aaaaaaaaaa/Controllers/AtoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aaaaaaaaaa.Controllers
+{
+    public class AtoController : Controller
+    {
+        // GET: Ato
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
