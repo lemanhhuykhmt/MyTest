f78fd223e8f83c69811f89f70d64c308195afe88
LEMANHHUY <habua2911@gmail.com>
2018-06-10 17:06:13 +0700

    dfdfdf

--- aaaaaaaaaa/Controllers/HomeController.cs
+++ aaaaaaaaaa/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace aaaaaaaaaa.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "lê mạnh huy";
 
             return View();
         }
